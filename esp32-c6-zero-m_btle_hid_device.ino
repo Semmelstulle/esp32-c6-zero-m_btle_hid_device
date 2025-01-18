b92bdccbc0b877695766f381670664149f980632
Semmelstulle <74740271+Semmelstulle@users.noreply.github.com>
2025-01-18 20:32:56 +0100

    test logic

--- esp32-c6-zero-m_btle_hid_device.ino
+++ esp32-c6-zero-m_btle_hid_device.ino
@@ -5,29 +5,69 @@
   is supposed to emulate a mouse vscroll and hscroll
 */
 
-// constants won't change. They're used here to set pin numbers:
-const int mouseClickPin = 0;  // the number of the pushbutton pin
+#include <BleKeyboard.h>
 
-// variables will change:
-int mouseClickPinState = 0;  // variable for reading the pushbutton status
+// this is the array the library takes to configure the device
+// containing (device_name, oem, battery_default_level)
+BleKeyboard bleKeyboard("ESP32 HID Dial", "Espressif", 100);
 
+// my mapping for the pins, prone to change
+const int upArrowPin = 0;
+const int downArrowPin = 1;
+const int enterKeyPin = 2;
+const int volumeUpPin = 3;
+const int volumeDownPin = 4;
+
+// setup code that runs before the loop
 void setup() {
-  // initialize the LED pin as an output:
-  pinMode(LED_BUILTIN, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(mouseClickPin, INPUT);
+  pinMode(upArrowPin, INPUT_PULLUP);
+  pinMode(downArrowPin, INPUT_PULLUP);
+  pinMode(enterKeyPin, INPUT_PULLUP);
+  pinMode(volumeUpPin, INPUT_PULLUP);
+  pinMode(volumeDownPin, INPUT_PULLUP);
+  
+  pinMode(LED_BUILTIN, OUTPUT); // on board led
+  
+  // calls a library function
+  bleKeyboard.begin();
 }
 
+// main loop
 void loop() {
-  // read the state of the pushbutton value:
-  mouseClickPinState = digitalRead(mouseClickPin);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (mouseClickPinState == HIGH) {
-    // turn LED on:
-    digitalWrite(LED_BUILTIN, HIGH);
-  } else {
-    // turn LED off:
-    digitalWrite(LED_BUILTIN, LOW);
+  // enables the reading of GPIO when it is connected
+  if (bleKeyboard.isConnected()) {
+    if (digitalRead(upArrowPin) == LOW) {
+      bleKeyboard.write(KEY_UP_ARROW);
+      blinkLED();
+    }
+    
+    if (digitalRead(downArrowPin) == LOW) {
+      bleKeyboard.write(KEY_DOWN_ARROW);
+      blinkLED();
+    }
+    
+    if (digitalRead(enterKeyPin) == LOW) {
+      bleKeyboard.write(KEY_RETURN);
+      blinkLED();
+    }
+    
+    if (digitalRead(volumeUpPin) == LOW) {
+      bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
+      blinkLED();
+    }
+    
+    if (digitalRead(volumeDownPin) == LOW) {
+      bleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
+      blinkLED();
+    }
   }
+  
+  delay(10);  // small delay to avoid bouncing
+}
+
+// visual feedback for button actions
+void blinkLED() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(50);
+  digitalWrite(LED_BUILTIN, LOW);
 }
